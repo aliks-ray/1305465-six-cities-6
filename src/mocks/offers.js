4531ac54d5355787737dc5fd889ccd8991f9d68f
Alexandra K <64307537+aliks-ray@users.noreply.github.com>
2021-02-15 18:26:53 +0300

    Добавлены данные для моков

---
+++ src/mocks/offers.js
@@ -0,0 +1,42 @@
+export const OFFERS = [
+  {
+    id: 1,
+    title: `Wood and stone place`,
+    type: `room`,
+    price: 80,
+    rating: 4.5,
+    previewImage: `./img/room.jpg`,
+    isFavorite: false,
+    isPremium: false
+  },
+  {
+    id: 2,
+    title: `Beautiful & luxurious studio at great location`,
+    type: `apartment`,
+    price: 120,
+    rating: 4.8,
+    previewImage: `./img/apartment-01.jpg`,
+    isFavorite: true,
+    isPremium: false
+  },
+  {
+    id: 3,
+    title: `Canal View Prinsengracht`,
+    type: `apartment`,
+    price: 132,
+    rating: 4.8,
+    previewImage: `./img/apartment-02.jpg`,
+    isFavorite: false,
+    isPremium: true
+  },
+  {
+    id: 4,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`,
+    price: 180,
+    rating: 4.8,
+    previewImage: `./img/apartment-03.jpg`,
+    isFavorite: true,
+    isPremium: true
+  }
+];
