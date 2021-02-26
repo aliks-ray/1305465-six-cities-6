c62813f1f1b6a02d84e6cc5f47fa57ffef7b68a4
Alexandra K <64307537+aliks-ray@users.noreply.github.com>
2021-02-26 23:17:03 +0300

    Правки после проверки

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -3,15 +3,13 @@ import PropTypes from "prop-types";
 import Review from "../review/review.jsx";
 import {reviewType} from "../../prop-types/prop-types.js";
 
-const ReviewsList = ({reviews}) => {
-  return (
-    <ul className="reviews__list">
-      {reviews.map((review) => {
-        return <Review key={review.id} review={review} />;
-      })}
-    </ul>
-  );
-};
+const ReviewsList = ({reviews}) => (
+  <ul className="reviews__list">
+    {reviews.map((review) => {
+      return <Review key={review.id} review={review} />;
+    })}
+  </ul>
+);
 
 ReviewsList.propTypes = {
   reviews: PropTypes.arrayOf(reviewType).isRequired
